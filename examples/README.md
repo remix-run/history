ba2bb1d3b33ed9351dde304525389949a2179af3
Michael Jackson <mjijackson@gmail.com>
2015-07-16 15:19:23 -0700

    Remove unnecessary files

--- examples/README.md
+++
@@ -1,9 +0,0 @@
-React Router Examples
-=====================
-
-To run and development examples:
-
-1. Clone this repo
-2. Run `npm install`
-3. Start the development server with `npm run examples`
-4. Point your browser to http://localhost:8080
--- modules/index.js
+++ modules/index.js
@@ -1,18 +1,5 @@
-/* components */
-export Router from './Router';
-export Link from './Link';
-
-/* components (configuration) */
-export Redirect from './Redirect';
-export Route from './Route';
-
-/* mixins */
-export Navigation from './Navigation';
-export TransitionHook from './TransitionHook';
-export State from './State';
-
-/* utils */
-export { createRoutesFromReactChildren } from './RouteUtils';
-export PropTypes from './PropTypes';
-
-export default from './Router';
+export History from './History';
+export BrowserHistory from './BrowserHistory';
+export HashHistory from './HashHistory';
+export MemoryHistory from './MemoryHistory';
+export Location from './Location';
