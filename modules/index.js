099fcfaca0539b4333fda0d4b350ca64e9248062
Michael Jackson <mjijackson@gmail.com>
2015-07-18 16:30:40 -0700

    Export NavigationTypes

--- modules/index.js
+++ modules/index.js
@@ -2,3 +2,5 @@ export createHistory from './createMemoryHistory';
 export createBrowserHistory from './createBrowserHistory';
 export createHashHistory from './createHashHistory';
 export createLocation from './createLocation';
+
+export NavigationTypes from './NavigationTypes';
