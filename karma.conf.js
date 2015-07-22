849e4bf124c7d73855342506ae2668793437c2ca
Michael Jackson <mjijackson@gmail.com>
2015-07-22 08:46:02 -0700

    Experimental karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,39 +1,58 @@
 var webpack = require('webpack');
 
 module.exports = function (config) {
-  var isTravis = !!process.env.TRAVIS;
-
-  // Browsers to run on Sauce Labs
-  var customLaunchers = {
-    'SL_Chrome': {
-      base: 'SauceLabs',
-      browserName: 'chrome'
-    },
-    'SL_InternetExplorer': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      version: '10'
-    },
-    'SL_Firefox': {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-    }
-  };
-
   config.set({
-    browserNoActivityTimeout: 120000,
-
-    browsers: isTravis ? Object.keys(customLaunchers) : [ 'Chrome' ],
+    browsers: [ 'Chrome' ],
+    browserNoActivityTimeout: 30000,
+    captureTimeout: 30000,
 
     sauceLabs: {
       testName: 'history'
     },
 
-    captureTimeout: 120000,
-
-    customLaunchers: customLaunchers,
-
-    singleRun: isTravis,
+    // Browsers to run on Sauce Labs
+    customLaunchers: {
+      'SL_Chrome': {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        version: '39'
+      },
+      'SL_Firefox': {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        version: '31'
+      },
+      'SL_Safari': {
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.10',
+        version: '8'
+      },
+      'SL_IE_9': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 2008',
+        version: '9'
+      },
+      'SL_IE_10': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 2012',
+        version: '10'
+      },
+      'SL_IE_11': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 8.1',
+        version: '11'
+      },
+      'SL_iOS': {
+        base: 'SauceLabs',
+        browserName: 'iphone',
+        platform: 'OS X 10.10',
+        version: '8.1'
+      }
+    },
 
     frameworks: [ 'mocha' ],
 
@@ -45,7 +64,7 @@ module.exports = function (config) {
       'tests.webpack.js': [ 'webpack', 'sourcemap' ]
     },
 
-    reporters: [ 'dots' ],
+    reporters: [ 'dots', 'saucelabs' ],
 
     webpack: {
       devtool: 'inline-source-map',
@@ -65,4 +84,25 @@ module.exports = function (config) {
       noInfo: true
     }
   });
+
+  if (process.env.TRAVIS) {
+    var buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
+
+    config.singleRun = true;
+
+    // Karma (with socket.io 1.x) buffers by 50 and 50 tests can take a long time on IEs
+    config.browserNoActivityTimeout = 120000;
+
+    config.sauceLabs.build = buildLabel;
+    config.sauceLabs.startConnect = false;
+    config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+    config.sauceLabs.recordScreenshots = false;
+
+    if (process.env.BROWSER_PROVIDER === 'saucelabs' || !process.env.BROWSER_PROVIDER) {
+      // Allocating a browser can take pretty long (eg. if we are out of capacity and need to wait
+      // for another build to finish) and so the `captureTimeout` typically kills
+      // an in-queue-pending request, which makes no sense.
+      config.captureTimeout = 0;
+    }
+  }
 };
