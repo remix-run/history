56662588bc5c67c2aa9dc159ba7d58e6d6aafc71
Michael Jackson <mjijackson@gmail.com>
2015-07-24 08:03:35 -0700

    More experimental karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,58 +1,56 @@
 var webpack = require('webpack');
 
 module.exports = function (config) {
+  // Browsers to run on Sauce Labs
+  var customLaunchers = {
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '39'
+    },
+    'SL_Firefox': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '31'
+    },
+    'SL_Safari': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.10',
+      version: '8'
+    },
+    'SL_IE_9': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2008',
+      version: '9'
+    },
+    'SL_IE_10': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2012',
+      version: '10'
+    },
+    'SL_IE_11': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    },
+    'SL_iOS': {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.10',
+      version: '8.1'
+    }
+  };
+
   config.set({
+    customLaunchers: customLaunchers,
+
     browsers: [ 'Chrome' ],
     browserNoActivityTimeout: 30000,
     captureTimeout: 30000,
-
-    sauceLabs: {
-      testName: 'history'
-    },
-
-    // Browsers to run on Sauce Labs
-    customLaunchers: {
-      'SL_Chrome': {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        version: '39'
-      },
-      'SL_Firefox': {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        version: '31'
-      },
-      'SL_Safari': {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.10',
-        version: '8'
-      },
-      'SL_IE_9': {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 2008',
-        version: '9'
-      },
-      'SL_IE_10': {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 2012',
-        version: '10'
-      },
-      'SL_IE_11': {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8.1',
-        version: '11'
-      },
-      'SL_iOS': {
-        base: 'SauceLabs',
-        browserName: 'iphone',
-        platform: 'OS X 10.10',
-        version: '8.1'
-      }
-    },
 
     frameworks: [ 'mocha' ],
 
@@ -86,23 +84,26 @@ module.exports = function (config) {
   });
 
   if (process.env.TRAVIS) {
-    var buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
-
-    config.singleRun = true;
+    config.browsers = Object.keys(customLaunchers);
 
     // Karma (with socket.io 1.x) buffers by 50 and 50 tests can take a long time on IEs
     config.browserNoActivityTimeout = 120000;
 
-    config.sauceLabs.build = buildLabel;
-    config.sauceLabs.startConnect = false;
-    config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
-    config.sauceLabs.recordScreenshots = false;
+    // Allocating a browser in Sauce Labs can take a while (e.g. if we are out of
+    // capacity and need to wait for another build to finish) so captureTimeout
+    // typically kills an in-queue-pending request, which makes no sense.
+    config.captureTimeout = 0;
 
-    if (process.env.BROWSER_PROVIDER === 'saucelabs' || !process.env.BROWSER_PROVIDER) {
-      // Allocating a browser can take pretty long (eg. if we are out of capacity and need to wait
-      // for another build to finish) and so the `captureTimeout` typically kills
-      // an in-queue-pending request, which makes no sense.
-      config.captureTimeout = 0;
-    }
+    var buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
+
+    config.sauceLabs = {
+      testName: 'history',
+      build: buildLabel,
+      startConnect: false,
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+      recordScreenshots: false
+    };
+
+    config.singleRun = true;
   }
 };
