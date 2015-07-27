7f429e2c29a52dea1fa0f2c3630389be2e0f8257
Michael Jackson <mjijackson@gmail.com>
2015-07-27 15:37:07 -0700

    Reduce code duplication

--- modules/__tests__/describeGo.js
+++ modules/__tests__/describeGo.js
@@ -3,7 +3,7 @@ import { PUSH, POP } from '../Actions';
 import execSteps from './execSteps';
 
 function describeGo(createHistory) {
-  describe('goBack', function () {
+  describe('go', function () {
     var history, unlisten;
     beforeEach(function () {
       history = createHistory();
@@ -14,82 +14,74 @@ function describeGo(createHistory) {
         unlisten();
     });
 
-    it('calls change listeners with the previous location', function (done) {
-      var steps = [
-        function (location) {
-          expect(location.pathname).toEqual('/');
-          expect(location.search).toEqual('');
-          expect(location.state).toEqual(null);
-          expect(location.action).toEqual(POP);
+    describe('back', function () {
+      it('calls change listeners with the previous location', function (done) {
+        var steps = [
+          function (location) {
+            expect(location.pathname).toEqual('/');
+            expect(location.search).toEqual('');
+            expect(location.state).toEqual(null);
+            expect(location.action).toEqual(POP);
 
-          history.pushState({ the: 'state' }, '/one?the=query');
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/one');
-          expect(location.search).toEqual('?the=query');
-          expect(location.state).toEqual({ the: 'state' });
-          expect(location.action).toEqual(PUSH);
+            history.pushState({ the: 'state' }, '/one?the=query');
+          },
+          function (location) {
+            expect(location.pathname).toEqual('/one');
+            expect(location.search).toEqual('?the=query');
+            expect(location.state).toEqual({ the: 'state' });
+            expect(location.action).toEqual(PUSH);
 
-          history.goBack();
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/');
-          expect(location.search).toEqual('');
-          expect(location.state).toEqual(null);
-          expect(location.action).toEqual(POP);
-        }
-      ];
+            history.goBack();
+          },
+          function (location) {
+            expect(location.pathname).toEqual('/');
+            expect(location.search).toEqual('');
+            expect(location.state).toEqual(null);
+            expect(location.action).toEqual(POP);
+          }
+        ];
 
-      unlisten = history.listen(execSteps(steps, done));
-    });
-  });
-
-  describe('goForward', function () {
-    var history, unlisten;
-    beforeEach(function () {
-      history = createHistory();
+        unlisten = history.listen(execSteps(steps, done));
+      });
     });
 
-    afterEach(function () {
-      if (unlisten)
-        unlisten();
-    });
+    describe('forward', function () {
+      it('calls change listeners with the next location', function (done) {
+        var steps = [
+          function (location) {
+            expect(location.pathname).toEqual('/');
+            expect(location.search).toEqual('');
+            expect(location.state).toEqual(null);
+            expect(location.action).toEqual(POP);
 
-    it('calls change listeners with the next location', function (done) {
-      var steps = [
-        function (location) {
-          expect(location.pathname).toEqual('/');
-          expect(location.search).toEqual('');
-          expect(location.state).toEqual(null);
-          expect(location.action).toEqual(POP);
+            history.pushState({ the: 'state' }, '/one?the=query');
+          },
+          function (location) {
+            expect(location.pathname).toEqual('/one');
+            expect(location.search).toEqual('?the=query');
+            expect(location.state).toEqual({ the: 'state' });
+            expect(location.action).toEqual(PUSH);
 
-          history.pushState({ the: 'state' }, '/one?the=query');
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/one');
-          expect(location.search).toEqual('?the=query');
-          expect(location.state).toEqual({ the: 'state' });
-          expect(location.action).toEqual(PUSH);
+            history.goBack();
+          },
+          function (location) {
+            expect(location.pathname).toEqual('/');
+            expect(location.search).toEqual('');
+            expect(location.state).toEqual(null);
+            expect(location.action).toEqual(POP);
 
-          history.goBack();
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/');
-          expect(location.search).toEqual('');
-          expect(location.state).toEqual(null);
-          expect(location.action).toEqual(POP);
+            history.goForward();
+          },
+          function (location) {
+            expect(location.pathname).toEqual('/one');
+            expect(location.search).toEqual('?the=query');
+            expect(location.state).toEqual({ the: 'state' });
+            expect(location.action).toEqual(POP);
+          }
+        ];
 
-          history.goForward();
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/one');
-          expect(location.search).toEqual('?the=query');
-          expect(location.state).toEqual({ the: 'state' });
-          expect(location.action).toEqual(POP);
-        }
-      ];
-
-      unlisten = history.listen(execSteps(steps, done));
+        unlisten = history.listen(execSteps(steps, done));
+      });
     });
   });
 }
