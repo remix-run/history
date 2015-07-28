fe2c96fc0a2cc4236f40468547cd59b30fc5389e
Michael Jackson <mjijackson@gmail.com>
2015-07-28 12:28:56 -0700

    Version 1.0.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -e
 
-changelog=node_modules/.bin/changelog
-
 update_version() {
   echo "$(node -p "p=require('./${1}');p.version='${2}';JSON.stringify(p,null,2)")" > $1
   echo "Updated ${1} version to ${2}"
+}
+
+validate_semver() {
+  if ! [[ $1 =~ ^[0-9]\.[0-9]+\.[0-9](-.+)? ]]; then
+    echo "Version $1 is not valid! It must be a valid semver string like 1.0.2 or 2.3.0-beta.1"
+    exit 1
+  fi
 }
 
 current_version=$(node -p "require('./package').version")
@@ -12,10 +17,7 @@ current_version=$(node -p "require('./package').version")
 printf "Next version (current is $current_version)? "
 read next_version
 
-if ! [[ $next_version =~ ^[0-9]\.[0-9]+\.[0-9](-.+)? ]]; then
-  echo "Version must be a valid semver string, e.g. 1.0.2 or 2.3.0-beta.1"
-  exit 1
-fi
+validate_semver $next_version
 
 next_ref="v$next_version"
 
@@ -23,10 +25,8 @@ npm test -- --single-run
 
 update_version 'package.json' $next_version
 
-$changelog -t $next_ref
-
 npm run build
-git add -A build
+git add -A lib
 
 git commit -am "Version $next_version"
 
@@ -37,4 +37,4 @@ git push origin master
 git push origin $next_ref
 git push origin latest -f
 
-npm publish build
+#npm publish
