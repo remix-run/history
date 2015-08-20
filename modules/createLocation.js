07a913e58f8f82e5c5873b2ad274c446c8cf7220
Michael Jackson <mjijackson@gmail.com>
2015-08-20 15:06:37 -0700

    Strip protocol + domain from Location paths
    Fixes #32

--- modules/createLocation.js
+++ modules/createLocation.js
@@ -1,6 +1,24 @@
+import warning from 'warning';
 import { POP } from './Actions';
 
+function extractPath(string) {
+  var match = string.match(/https?:\/\/[^\/]*/);
+
+  if (match == null)
+    return string;
+
+  warning(
+    false,
+    'Location path must be pathname + query string only, not a fully qualified URL like "%s"',
+    string
+  );
+
+  return string.substring(match[0].length);
+}
+
 function createLocation(path='/', state=null, action=POP, key=null) {
+  path = extractPath(path);
+
   var index = path.indexOf('?');
 
   var pathname, search;
