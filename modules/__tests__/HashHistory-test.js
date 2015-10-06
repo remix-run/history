6ed20f57ed8f5c8fe215bbdebf30f358cfc5979f
Michael Jackson <mjijackson@gmail.com>
2015-10-06 09:53:42 -0700

    Refactor test

--- modules/__tests__/HashHistory-test.js
+++ modules/__tests__/HashHistory-test.js
@@ -1,116 +1,15 @@
 /*eslint-env mocha */
 import expect from 'expect'
-import { PUSH, POP } from '../Actions'
 import { supportsGoWithoutReloadUsingHash } from '../DOMUtils'
 import createHashHistory from '../createHashHistory'
 import describeInitialLocation from './describeInitialLocation'
 import describeTransitions from './describeTransitions'
 import describePushState from './describePushState'
 import describeReplaceState from './describeReplaceState'
+import describeQueryKey from './describeQueryKey'
 import describeBasename from './describeBasename'
 import describeQueries from './describeQueries'
 import describeGo from './describeGo'
-import execSteps from './execSteps'
-
-function describeStatePersistence(createHistory) {
-  describe('when the user does not want to persist a state', function () {
-    let history, unlisten
-    beforeEach(function () {
-      history = createHistory({ queryKey: false })
-    })
-
-    afterEach(function () {
-      if (unlisten)
-        unlisten()
-    })
-
-    it('forgets state across transitions', function (done) {
-      const steps = [
-        function (location) {
-          expect(location.pathname).toEqual('/')
-          expect(location.search).toEqual('')
-          expect(location.state).toEqual(null)
-          expect(location.action).toEqual(POP)
-
-          history.pushState(null, '/home?the=query')
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/home')
-          expect(location.search).toEqual('?the=query')
-          expect(location.state).toEqual(null)
-          expect(location.action).toEqual(PUSH)
-
-          history.goBack()
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/')
-          expect(location.search).toEqual('')
-          expect(location.state).toEqual(null)
-          expect(location.action).toEqual(POP)
-
-          history.goForward()
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/home')
-          expect(location.search).toEqual('?the=query')
-          expect(location.state).toEqual(null) // State is missing.
-          expect(location.action).toEqual(POP)
-        }
-      ]
-
-      unlisten = history.listen(execSteps(steps, done))
-    })
-  })
-
-  describe('when the user wants to persist state', function () {
-    let history, unlisten
-    beforeEach(function () {
-      history = createHistory({ queryKey: 'a' })
-    })
-
-    afterEach(function () {
-      if (unlisten)
-        unlisten()
-    })
-
-    it('remembers state across transitions', function (done) {
-      const steps = [
-        function (location) {
-          expect(location.pathname).toEqual('/')
-          expect(location.search).toEqual('')
-          expect(location.state).toEqual(null)
-          expect(location.action).toEqual(POP)
-
-          history.pushState({ the: 'state' }, '/home?the=query')
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/home')
-          expect(location.search).toEqual('?the=query')
-          expect(location.state).toEqual({ the: 'state' })
-          expect(location.action).toEqual(PUSH)
-
-          history.goBack()
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/')
-          expect(location.search).toEqual('')
-          expect(location.state).toEqual(null)
-          expect(location.action).toEqual(POP)
-
-          history.goForward()
-        },
-        function (location) {
-          expect(location.pathname).toEqual('/home')
-          expect(location.search).toEqual('?the=query')
-          expect(location.state).toEqual({ the: 'state' }) // State is present.
-          expect(location.action).toEqual(POP)
-        }
-      ]
-
-      unlisten = history.listen(execSteps(steps, done))
-    })
-  })
-}
 
 describe('hash history', function () {
   beforeEach(function () {
@@ -127,11 +26,11 @@ describe('hash history', function () {
 
   if (supportsGoWithoutReloadUsingHash()) {
     describeGo(createHashHistory)
-    describeStatePersistence(createHashHistory)
+    describeQueryKey(createHashHistory)
   } else {
     describe.skip(null, function () {
       describeGo(createHashHistory)
-      describeStatePersistence(createHashHistory)
+      describeQueryKey(createHashHistory)
     })
   }
 
---
+++ modules/__tests__/describeQueryKey.js
@@ -0,0 +1,106 @@
+/*eslint-env mocha */
+import expect from 'expect'
+import { PUSH, POP } from '../Actions'
+import execSteps from './execSteps'
+
+function describeQueryKey(createHistory) {
+  describe('when the user does not want to persist a state', function () {
+    let history, unlisten
+    beforeEach(function () {
+      history = createHistory({ queryKey: false })
+    })
+
+    afterEach(function () {
+      if (unlisten)
+        unlisten()
+    })
+
+    it('forgets state across transitions', function (done) {
+      const steps = [
+        function (location) {
+          expect(location.pathname).toEqual('/')
+          expect(location.search).toEqual('')
+          expect(location.state).toEqual(null)
+          expect(location.action).toEqual(POP)
+
+          history.pushState(null, '/home?the=query')
+        },
+        function (location) {
+          expect(location.pathname).toEqual('/home')
+          expect(location.search).toEqual('?the=query')
+          expect(location.state).toEqual(null)
+          expect(location.action).toEqual(PUSH)
+
+          history.goBack()
+        },
+        function (location) {
+          expect(location.pathname).toEqual('/')
+          expect(location.search).toEqual('')
+          expect(location.state).toEqual(null)
+          expect(location.action).toEqual(POP)
+
+          history.goForward()
+        },
+        function (location) {
+          expect(location.pathname).toEqual('/home')
+          expect(location.search).toEqual('?the=query')
+          expect(location.state).toEqual(null) // State is missing.
+          expect(location.action).toEqual(POP)
+        }
+      ]
+
+      unlisten = history.listen(execSteps(steps, done))
+    })
+  })
+
+  describe('when the user wants to persist state', function () {
+    let history, unlisten
+    beforeEach(function () {
+      history = createHistory({ queryKey: 'a' })
+    })
+
+    afterEach(function () {
+      if (unlisten)
+        unlisten()
+    })
+
+    it('remembers state across transitions', function (done) {
+      const steps = [
+        function (location) {
+          expect(location.pathname).toEqual('/')
+          expect(location.search).toEqual('')
+          expect(location.state).toEqual(null)
+          expect(location.action).toEqual(POP)
+
+          history.pushState({ the: 'state' }, '/home?the=query')
+        },
+        function (location) {
+          expect(location.pathname).toEqual('/home')
+          expect(location.search).toEqual('?the=query')
+          expect(location.state).toEqual({ the: 'state' })
+          expect(location.action).toEqual(PUSH)
+
+          history.goBack()
+        },
+        function (location) {
+          expect(location.pathname).toEqual('/')
+          expect(location.search).toEqual('')
+          expect(location.state).toEqual(null)
+          expect(location.action).toEqual(POP)
+
+          history.goForward()
+        },
+        function (location) {
+          expect(location.pathname).toEqual('/home')
+          expect(location.search).toEqual('?the=query')
+          expect(location.state).toEqual({ the: 'state' }) // State is present.
+          expect(location.action).toEqual(POP)
+        }
+      ]
+
+      unlisten = history.listen(execSteps(steps, done))
+    })
+  })
+}
+
+export default describeQueryKey
