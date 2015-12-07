564197a26a0131edbbebbebe69ec9b57f862cac1
Markku Rontu <markku.rontu@iki.fi>
2015-12-07 16:22:19 +0200

    Use forced REPLACE only when both location and state are the same
    - Update tests to include check for changing state
    - URL stays the same, but state changes.

--- modules/__tests__/describePushState.js
+++ modules/__tests__/describePushState.js
@@ -51,13 +51,42 @@ function describePushState(createHistory) {
           expect(location.state).toEqual({ the: 'state' })
           expect(location.action).toEqual(PUSH)
 
+          history.pushState({ the: 'state' }, '/home?the=query')
+        },
+        function (location) {
+          expect(location.pathname).toEqual('/home')
+          expect(location.search).toEqual('?the=query')
+          expect(location.state).toEqual({ the: 'state' })
+          expect(location.action).toEqual(REPLACE)
+        }
+      ]
+
+      unlisten = history.listen(execSteps(steps, done))
+    })
+
+    it('stays PUSH if state is changed', function (done) {
+      let steps = [
+        function (location) {
+          expect(location.pathname).toEqual('/')
+          expect(location.search).toEqual('')
+          expect(location.state).toEqual(null)
+          expect(location.action).toEqual(POP)
+
+          history.pushState({ the: 'state' }, '/home?the=query')
+        },
+        function (location) {
+          expect(location.pathname).toEqual('/home')
+          expect(location.search).toEqual('?the=query')
+          expect(location.state).toEqual({ the: 'state' })
+          expect(location.action).toEqual(PUSH)
+
           history.pushState({ different: 'state' }, '/home?the=query')
         },
         function (location) {
           expect(location.pathname).toEqual('/home')
           expect(location.search).toEqual('?the=query')
           expect(location.state).toEqual({ different: 'state' })
-          expect(location.action).toEqual(REPLACE)
+          expect(location.action).toEqual(PUSH)
         }
       ]
 
