47ea031d6e37eb942d7f166f655ca3c233d4614a
Michael Jackson <mjijackson@gmail.com>
2016-03-02 19:59:59 -0800

    Update build/release scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,13 @@
-var execSync = require('child_process').execSync
-var readFileSync = require('fs').readFileSync
-var prettyBytes = require('pretty-bytes')
-var gzipSize = require('gzip-size')
+const readFileSync = require('fs').readFileSync
+const execSync = require('child_process').execSync
+const prettyBytes = require('pretty-bytes')
+const gzipSize = require('gzip-size')
 
-function exec(command) {
-  execSync(command, { stdio: [0, 1, 2] })
-}
+const exec = (command) =>
+  execSync(command, { stdio: 'inherit' })
 
-exec('npm run build')
+exec('npm run build-cjs')
+exec('npm run build-es6')
 exec('npm run build-umd')
 exec('npm run build-min')
 
