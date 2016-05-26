eac49177415a575cd40a087e4e3ff027669c3cc3
Michael Jackson <mjijackson@gmail.com>
2016-05-25 18:13:55 -0700

    Fix builds on Windows
    Fixes #300

--- scripts/build.js
+++ scripts/build.js
@@ -3,13 +3,17 @@ const execSync = require('child_process').execSync
 const prettyBytes = require('pretty-bytes')
 const gzipSize = require('gzip-size')
 
-const exec = (command) =>
-  execSync(command, { stdio: 'inherit' })
+const exec = (command, env) =>
+  execSync(command, { stdio: 'inherit', env })
+
+const webpackEnv = Object.assign({}, process.env, {
+  NODE_ENV: 'production'
+})
 
 exec('npm run build-cjs')
 exec('npm run build-es6')
-exec('npm run build-umd')
-exec('npm run build-min')
+exec('npm run build-umd', webpackEnv)
+exec('npm run build-min', webpackEnv)
 
 console.log(
   '\ngzipped, the UMD build is ' + prettyBytes(
