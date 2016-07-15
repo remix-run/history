9bdbbe72e810c0e643c8a7532f6c82d726264970
Jimmy Jia <tesrin@gmail.com>
2016-07-14 20:27:26 -0400

    Work around SecurityError: DOM Exception 18 on Safari

--- modules/__tests__/describeBasename.js
+++ modules/__tests__/describeBasename.js
@@ -10,188 +10,213 @@ const stripHash = (path) =>
 
 const describeBasename = (createHistory) => {
   describe('basename handling', () => {
-    [
-      ['useBasename', () => {
+    let history
+    beforeEach(() => {
+      history = withBasename(createHistory(), '/base/url')
+    })
+
+    describe('in push', () => {
+      it('works with string', (done) => {
+        const steps = [
+          (location) => {
+            expect(location.pathname).toEqual('/')
+            expect(location.search).toEqual('')
+            expect(location.state).toBe(undefined)
+            expect(location.action).toEqual(POP)
+            expect(location.key).toBe(null)
+            expect(location.basename).toEqual('')
+
+            history.push('/home')
+          },
+          (location) => {
+            expect(location.pathname).toEqual('/home')
+            expect(location.search).toEqual('')
+            expect(location.state).toBe(undefined)
+            expect(location.action).toEqual(PUSH)
+            expect(location.key).toExist()
+            expect(location.basename).toEqual('/base/url')
+          }
+        ]
+
+        execSteps(steps, history, done)
+      })
+
+      it('works with object', (done) => {
+        const steps = [
+          (location) => {
+            expect(location.pathname).toEqual('/')
+            expect(location.search).toEqual('')
+            expect(location.state).toBe(undefined)
+            expect(location.action).toEqual(POP)
+            expect(location.key).toBe(null)
+            expect(location.basename).toEqual('')
+
+            history.push({
+              pathname: '/home',
+              state: { the: 'state' }
+            })
+          },
+          (location) => {
+            expect(location.pathname).toEqual('/home')
+            expect(location.search).toEqual('')
+            expect(location.state).toEqual({ the: 'state' })
+            expect(location.action).toEqual(PUSH)
+            expect(location.key).toExist()
+            expect(location.basename).toEqual('/base/url')
+
+            history.push({
+              ...location,
+              pathname: '/foo'
+            })
+          },
+          (location) => {
+            expect(location.pathname).toEqual('/foo')
+            expect(location.search).toEqual('')
+            expect(location.state).toEqual({ the: 'state' })
+            expect(location.action).toEqual(PUSH)
+            expect(location.key).toExist()
+            expect(location.basename).toEqual('/base/url')
+          }
+        ]
+
+        execSteps(steps, history, done)
+      })
+    })
+
+    describe('in replace', () => {
+      it('works with string', (done) => {
+        const steps = [
+          (location) => {
+            expect(location.pathname).toEqual('/')
+            expect(location.search).toEqual('')
+            expect(location.state).toBe(undefined)
+            expect(location.action).toEqual(POP)
+            expect(location.key).toBe(null)
+            expect(location.basename).toEqual('')
+
+            history.replace('/home')
+          },
+          (location) => {
+            expect(location.pathname).toEqual('/home')
+            expect(location.search).toEqual('')
+            expect(location.state).toBe(undefined)
+            expect(location.action).toEqual(REPLACE)
+            expect(location.key).toExist()
+            expect(location.basename).toEqual('/base/url')
+          }
+        ]
+
+        execSteps(steps, history, done)
+      })
+
+      it('works with object', (done) => {
+        const steps = [
+          (location) => {
+            expect(location.pathname).toEqual('/')
+            expect(location.search).toEqual('')
+            expect(location.state).toBe(undefined)
+            expect(location.action).toEqual(POP)
+            expect(location.key).toBe(null)
+            expect(location.basename).toEqual('')
+
+            history.replace({
+              pathname: '/home',
+              state: { the: 'state' }
+            })
+          },
+          (location) => {
+            expect(location.pathname).toEqual('/home')
+            expect(location.search).toEqual('')
+            expect(location.state).toEqual({ the: 'state' })
+            expect(location.action).toEqual(REPLACE)
+            expect(location.key).toExist()
+            expect(location.basename).toEqual('/base/url')
+
+            history.replace({
+              ...location,
+              pathname: '/foo'
+            })
+          },
+          (location) => {
+            expect(location.pathname).toEqual('/foo')
+            expect(location.search).toEqual('')
+            expect(location.state).toEqual({ the: 'state' })
+            expect(location.action).toEqual(REPLACE)
+            expect(location.key).toExist()
+            expect(location.basename).toEqual('/base/url')
+          }
+        ]
+
+        execSteps(steps, history, done)
+      })
+    })
+
+    describe('in createPath', () => {
+      it('works', () => {
+        expect(
+          history.createPath('/the/path')
+        ).toEqual('/base/url/the/path')
+      })
+    })
+
+    describe('in createHref', () => {
+      it('works', () => {
+        expect(
+          stripHash(history.createHref('/the/path'))
+        ).toEqual('/base/url/the/path')
+      })
+    })
+
+    describe('in createLocation', () => {
+      it('works with string', () => {
+        const location = history.createLocation('/the/path')
+
+        expect(location.pathname).toEqual('/the/path')
+        expect(location.basename).toEqual('/base/url')
+      })
+
+      it('works with object without query', () => {
+        const location = history.createLocation({
+          pathname: '/the/path'
+        })
+
+        expect(location.pathname).toEqual('/the/path')
+        expect(location.basename).toEqual('/base/url')
+      })
+    })
+  })
+
+  describe('useBasename', () => {
+    describe('basename handling', () => {
+      it('works', (done) => {
         shouldWarn('deprecated')
-        return useBasename(createHistory)({ basename: '/base/url' })
-      }],
-      ['withQueries', () => withBasename(createHistory(), '/base/url')]
-    ].forEach(([label, createEnhancedHistory]) => describe(label, () => {
-      let history
-      beforeEach(() => {
-        history = createEnhancedHistory()
+
+        const history = useBasename(createHistory)({ basename: '/base/url' })
+
+        const steps = [
+          (location) => {
+            expect(location.pathname).toEqual('/')
+            expect(location.search).toEqual('')
+            expect(location.state).toBe(undefined)
+            expect(location.action).toEqual(POP)
+            expect(location.key).toBe(null)
+            expect(location.basename).toEqual('')
+
+            history.push('/home')
+          },
+          (location) => {
+            expect(location.pathname).toEqual('/home')
+            expect(location.search).toEqual('')
+            expect(location.state).toBe(undefined)
+            expect(location.action).toEqual(PUSH)
+            expect(location.key).toExist()
+            expect(location.basename).toEqual('/base/url')
+          }
+        ]
+
+        execSteps(steps, history, done)
       })
-
-      describe('in push', () => {
-        it('works with string', (done) => {
-          const steps = [
-            (location) => {
-              expect(location.pathname).toEqual('/')
-              expect(location.search).toEqual('')
-              expect(location.state).toBe(undefined)
-              expect(location.action).toEqual(POP)
-              expect(location.key).toBe(null)
-              expect(location.basename).toEqual('')
-
-              history.push('/home')
-            },
-            (location) => {
-              expect(location.pathname).toEqual('/home')
-              expect(location.search).toEqual('')
-              expect(location.state).toBe(undefined)
-              expect(location.action).toEqual(PUSH)
-              expect(location.key).toExist()
-              expect(location.basename).toEqual('/base/url')
-            }
-          ]
-
-          execSteps(steps, history, done)
-        })
-
-        it('works with object', (done) => {
-          const steps = [
-            (location) => {
-              expect(location.pathname).toEqual('/')
-              expect(location.search).toEqual('')
-              expect(location.state).toBe(undefined)
-              expect(location.action).toEqual(POP)
-              expect(location.key).toBe(null)
-              expect(location.basename).toEqual('')
-
-              history.push({
-                pathname: '/home',
-                state: { the: 'state' }
-              })
-            },
-            (location) => {
-              expect(location.pathname).toEqual('/home')
-              expect(location.search).toEqual('')
-              expect(location.state).toEqual({ the: 'state' })
-              expect(location.action).toEqual(PUSH)
-              expect(location.key).toExist()
-              expect(location.basename).toEqual('/base/url')
-
-              history.push({
-                ...location,
-                pathname: '/foo'
-              })
-            },
-            (location) => {
-              expect(location.pathname).toEqual('/foo')
-              expect(location.search).toEqual('')
-              expect(location.state).toEqual({ the: 'state' })
-              expect(location.action).toEqual(PUSH)
-              expect(location.key).toExist()
-              expect(location.basename).toEqual('/base/url')
-            }
-          ]
-
-          execSteps(steps, history, done)
-        })
-      })
-
-      describe('in replace', () => {
-        it('works with string', (done) => {
-          const steps = [
-            (location) => {
-              expect(location.pathname).toEqual('/')
-              expect(location.search).toEqual('')
-              expect(location.state).toBe(undefined)
-              expect(location.action).toEqual(POP)
-              expect(location.key).toBe(null)
-              expect(location.basename).toEqual('')
-
-              history.replace('/home')
-            },
-            (location) => {
-              expect(location.pathname).toEqual('/home')
-              expect(location.search).toEqual('')
-              expect(location.state).toBe(undefined)
-              expect(location.action).toEqual(REPLACE)
-              expect(location.key).toExist()
-              expect(location.basename).toEqual('/base/url')
-            }
-          ]
-
-          execSteps(steps, history, done)
-        })
-
-        it('works with object', (done) => {
-          const steps = [
-            (location) => {
-              expect(location.pathname).toEqual('/')
-              expect(location.search).toEqual('')
-              expect(location.state).toBe(undefined)
-              expect(location.action).toEqual(POP)
-              expect(location.key).toBe(null)
-              expect(location.basename).toEqual('')
-
-              history.replace({
-                pathname: '/home',
-                state: { the: 'state' }
-              })
-            },
-            (location) => {
-              expect(location.pathname).toEqual('/home')
-              expect(location.search).toEqual('')
-              expect(location.state).toEqual({ the: 'state' })
-              expect(location.action).toEqual(REPLACE)
-              expect(location.key).toExist()
-              expect(location.basename).toEqual('/base/url')
-
-              history.replace({
-                ...location,
-                pathname: '/foo'
-              })
-            },
-            (location) => {
-              expect(location.pathname).toEqual('/foo')
-              expect(location.search).toEqual('')
-              expect(location.state).toEqual({ the: 'state' })
-              expect(location.action).toEqual(REPLACE)
-              expect(location.key).toExist()
-              expect(location.basename).toEqual('/base/url')
-            }
-          ]
-
-          execSteps(steps, history, done)
-        })
-      })
-
-      describe('in createPath', () => {
-        it('works', () => {
-          expect(
-            history.createPath('/the/path')
-          ).toEqual('/base/url/the/path')
-        })
-      })
-
-      describe('in createHref', () => {
-        it('works', () => {
-          expect(
-            stripHash(history.createHref('/the/path'))
-          ).toEqual('/base/url/the/path')
-        })
-      })
-
-      describe('in createLocation', () => {
-        it('works with string', () => {
-          const location = history.createLocation('/the/path')
-
-          expect(location.pathname).toEqual('/the/path')
-          expect(location.basename).toEqual('/base/url')
-        })
-
-        it('works with object without query', () => {
-          const location = history.createLocation({
-            pathname: '/the/path'
-          })
-
-          expect(location.pathname).toEqual('/the/path')
-          expect(location.basename).toEqual('/base/url')
-        })
-      })
-    }))
+    })
   })
 }
 
