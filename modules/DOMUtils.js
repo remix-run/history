00ac6888b78155c0215308f3a50ac54b62bffc41
Jonathan Rajavuori <jrajav@gmail.com>
2016-07-22 13:58:01 -0500

    Expand user-agent matching to include IE10 (and below) since it also seems to fail

--- modules/DOMUtils.js
+++ modules/DOMUtils.js
@@ -1,3 +1,5 @@
+const ua = window.navigator.userAgent
+
 export const addEventListener = (node, event, listener) =>
   node.addEventListener
     ? node.addEventListener(event, listener, false)
@@ -16,8 +18,6 @@ export const removeEventListener = (node, event, listener) =>
  * changed to avoid false negatives for Windows Phones: https://github.com/reactjs/react-router/issues/586
  */
 export const supportsHistory = () => {
-  const ua = window.navigator.userAgent
-
   if ((ua.indexOf('Android 2.') !== -1 || ua.indexOf('Android 4.0') !== -1) &&
     ua.indexOf('Mobile Safari') !== -1 &&
     ua.indexOf('Chrome') === -1 &&
@@ -32,10 +32,11 @@ export const supportsHistory = () => {
  * Returns false if using go(n) with hash history causes a full page reload.
  */
 export const supportsGoWithoutReloadUsingHash = () =>
-  window.navigator.userAgent.indexOf('Firefox') === -1
+  ua.indexOf('Firefox') === -1
 
 /**
- * Returns true if browser fires popstate on hash change
+ * Returns true if browser fires popstate on hash change.
+ * IE10 and IE11 do not.
  */
 export const supportsPopstateOnHashchange = () =>
-  !navigator.userAgent.match(/Trident.*rv[ :]*11\./)
+  ua.indexOf('Trident') !== -1
