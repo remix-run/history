0575bbe79d6517e7223d961c8c759ca73028f8d7
Jonathan Rajavuori <jrajav@gmail.com>
2016-07-22 14:50:11 -0500

    Undo 'ua' caching to avoid Node reference error

--- modules/DOMUtils.js
+++ modules/DOMUtils.js
@@ -1,5 +1,3 @@
-const ua = window.navigator.userAgent
-
 export const addEventListener = (node, event, listener) =>
   node.addEventListener
     ? node.addEventListener(event, listener, false)
@@ -18,6 +16,8 @@ export const removeEventListener = (node, event, listener) =>
  * changed to avoid false negatives for Windows Phones: https://github.com/reactjs/react-router/issues/586
  */
 export const supportsHistory = () => {
+  const ua = window.navigator.userAgent
+
   if ((ua.indexOf('Android 2.') !== -1 || ua.indexOf('Android 4.0') !== -1) &&
     ua.indexOf('Mobile Safari') !== -1 &&
     ua.indexOf('Chrome') === -1 &&
@@ -32,11 +32,11 @@ export const supportsHistory = () => {
  * Returns false if using go(n) with hash history causes a full page reload.
  */
 export const supportsGoWithoutReloadUsingHash = () =>
-  ua.indexOf('Firefox') === -1
+  window.navigator.userAgent.indexOf('Firefox') === -1
 
 /**
  * Returns true if browser fires popstate on hash change.
  * IE10 and IE11 do not.
  */
 export const supportsPopstateOnHashchange = () =>
-  ua.indexOf('Trident') === -1
+  window.navigator.userAgent.indexOf('Trident') === -1
