817af4423e76fa71db95fd094881b39abbfd99fe
Michael Jackson <mjijackson@gmail.com>
2017-01-09 12:55:22 -0800

    Fix unsubscribe bug
    Fixes a bug that allowed a listener to still be called if it was
    unregistered in another listener.

--- modules/createTransitionManager.js
+++ modules/createTransitionManager.js
@@ -46,16 +46,25 @@ const createTransitionManager = () => {
 
   let listeners = []
 
-  const appendListener = (listener) => {
+  const appendListener = (fn) => {
+    let isActive = true
+
+    const listener = (...args) => {
+      if (isActive)
+        fn(...args)
+    }
+
     listeners.push(listener)
 
     return () => {
+      isActive = false
       listeners = listeners.filter(item => item !== listener)
     }
   }
 
-  const notifyListeners = (...args) =>
+  const notifyListeners = (...args) => {
     listeners.forEach(listener => listener(...args))
+  }
 
   return {
     setPrompt,
