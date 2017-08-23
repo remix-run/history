1bbd58775a13ad1253a6a42e923a57c57f9ba806
MICHAEL JACKSON <mjijackson@gmail.com>
2017-08-23 00:03:34 -0700

    Build ES modules into package root

--- tools/build.js
+++ tools/build.js
@@ -13,16 +13,16 @@ const exec = (command, extraEnv) =>
     env: Object.assign({}, process.env, extraEnv)
   })
 
-console.log('Building CommonJS modules ...')
-
-exec('babel modules -d . --ignore __tests__', {
-  BABEL_ENV: 'cjs'
-})
-
 console.log('\nBuilding ES modules ...')
 
-exec('babel modules -d es --ignore __tests__', {
+exec('babel modules -d . --ignore __tests__', {
   BABEL_ENV: 'es'
+})
+
+console.log('Building CommonJS modules ...')
+
+exec('babel modules -d cjs --ignore __tests__', {
+  BABEL_ENV: 'cjs'
 })
 
 console.log('\nBuilding history.js ...')
--- tools/es2015-preset.js
+++ tools/es2015-preset.js
@@ -1,12 +1,8 @@
-const buildPreset = require('babel-preset-es2015').buildPreset
-
-const BABEL_ENV = process.env.BABEL_ENV
-
 module.exports = {
   presets: [
-    [ buildPreset, {
+    [ 'es2015', {
       loose: true,
-      modules: BABEL_ENV === 'es' ? false : 'commonjs'
+      modules: process.env.BABEL_ENV === 'es' ? false : 'commonjs'
     } ]
   ]
 }
