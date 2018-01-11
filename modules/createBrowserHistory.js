a181547cc32ceb0c1ba46d8bf7f5a5d5c62185cb
Guido Bouman <guido@q42.nl>
2018-01-11 14:39:17 +0100

    Clean whitespace.

--- modules/createBrowserHistory.js
+++ modules/createBrowserHistory.js
@@ -164,7 +164,7 @@ const createBrowserHistory = (props = {}) => {
 
     const action = "PUSH"
     const location = createLocation(path, state, createKey(), history.location)
-    
+
     if (createPath(location) === createPath(history.location)) {
       replace(path, state)
       return
