6ff17d09282ee8b740fe865e0207c75a87352d48
kimkimhun3 <kimkimhun3@gmail.com>
2019-02-18 15:11:00 +0700

    add file js

---
+++ history.js
@@ -0,0 +1,4 @@
+
+As of 2006, the latest version of the language is JavaScript 1.7, which corresponds to ECMA-262 Edition 3 like JavaScript 1.5, except for Array extras, Array and String generics, and pythonic generators 
+and array comprehensions. ECMAScript, in simple terms, is a standardized version of JavaScript. The ECMA-357 standard specifies E4X (ECMAscript For XML), a language extension dealing with XML.
+
