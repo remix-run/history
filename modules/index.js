b141a1bc7b6490a4012cdcbabf841b76cc6d9496
Michael Jackson <michael@jackson.us>
2019-09-06 16:19:04 -0700

    Revert "Deprecate createLocation and locationsAreEqual"
    This reverts commit 2c7250baa55cc99bc19233f87e6853a3596dc8eb.

--- modules/index.js
+++ modules/index.js
@@ -1,24 +1,5 @@
-export { default as createBrowserHistory } from './createBrowserHistory.js';
-export { default as createHashHistory } from './createHashHistory.js';
-export { default as createMemoryHistory } from './createMemoryHistory.js';
-export { parsePath, createPath } from './PathUtils.js';
-
-import deprecate from './deprecate.js';
-import { createLocation, locationsAreEqual } from './LocationUtils.js';
-
-const deprecatedCreateLocation = deprecate(
-  createLocation,
-  'createLocation is deprecated and will be removed in the next major release.' +
-    ' To create a location object from a URL string, use `location = parsePath(url)` instead.'
-);
-
-const deprecatedLocationsAreEqual = deprecate(
-  locationsAreEqual,
-  'locationsAreEqual is deprecated and will be removed in the next major release.' +
-    ' To check if two location objects represent the same location, compare `locationA.key === locationB.key` instead.'
-);
-
-export {
-  deprecatedCreateLocation as createLocation,
-  deprecatedLocationsAreEqual as locationsAreEqual
-};
+export { default as createBrowserHistory } from './createBrowserHistory';
+export { default as createHashHistory } from './createHashHistory';
+export { default as createMemoryHistory } from './createMemoryHistory';
+export { createLocation, locationsAreEqual } from './LocationUtils';
+export { parsePath, createPath } from './PathUtils';
