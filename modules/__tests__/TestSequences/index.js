1c2a1fe82638c7809f1c760d0103208d7c7af718
Michael Jackson <michael@jackson.us>
2019-09-11 13:41:05 -0700

    Use .js extension for relative imports

--- modules/__tests__/TestSequences/index.js
+++ modules/__tests__/TestSequences/index.js
@@ -1,46 +1,46 @@
 export {
   default as BackButtonTransitionHook
-} from './BackButtonTransitionHook';
-export { default as BlockEverything } from './BlockEverything';
+} from './BackButtonTransitionHook.js';
+export { default as BlockEverything } from './BlockEverything.js';
 export {
   default as BlockPopWithoutListening
-} from './BlockPopWithoutListening';
-export { default as DenyPush } from './DenyPush';
-export { default as DenyGoBack } from './DenyGoBack';
-export { default as DenyGoForward } from './DenyGoForward';
+} from './BlockPopWithoutListening.js';
+export { default as DenyPush } from './DenyPush.js';
+export { default as DenyGoBack } from './DenyGoBack.js';
+export { default as DenyGoForward } from './DenyGoForward.js';
 export {
   default as EncodedReservedCharacters
-} from './EncodedReservedCharacters';
-export { default as GoBack } from './GoBack';
-export { default as GoForward } from './GoForward';
-export { default as HashbangHashPathCoding } from './HashbangHashPathCoding';
+} from './EncodedReservedCharacters.js';
+export { default as GoBack } from './GoBack.js';
+export { default as GoForward } from './GoForward.js';
+export { default as HashbangHashPathCoding } from './HashbangHashPathCoding.js';
 export {
   default as HashChangeTransitionHook
-} from './HashChangeTransitionHook';
-export { default as InitialLocationNoKey } from './InitialLocationNoKey';
-export { default as InitialLocationHasKey } from './InitialLocationHasKey';
-export { default as Listen } from './Listen';
+} from './HashChangeTransitionHook.js';
+export { default as InitialLocationNoKey } from './InitialLocationNoKey.js';
+export { default as InitialLocationHasKey } from './InitialLocationHasKey.js';
+export { default as Listen } from './Listen.js';
 export {
   default as LocationPathnameAlwaysSame
-} from './LocationPathnameAlwaysSame';
-export { default as NoslashHashPathCoding } from './NoslashHashPathCoding';
-export { default as PushEncodedLocation } from './PushEncodedLocation';
-export { default as PushInvalidPathname } from './PushInvalidPathname';
-export { default as PushNewLocation } from './PushNewLocation';
-export { default as PushMissingPathname } from './PushMissingPathname';
-export { default as PushSamePath } from './PushSamePath';
-export { default as PushSamePathWarning } from './PushSamePathWarning';
-export { default as PushState } from './PushState';
-export { default as PushStateWarning } from './PushStateWarning';
-export { default as PushRelativePathname } from './PushRelativePathname';
-export { default as PushUnicodeLocation } from './PushUnicodeLocation';
-export { default as ReplaceInvalidPathname } from './ReplaceInvalidPathname';
-export { default as ReplaceNewLocation } from './ReplaceNewLocation';
-export { default as ReplaceSamePath } from './ReplaceSamePath';
-export { default as ReplaceState } from './ReplaceState';
-export { default as ReplaceStateWarning } from './ReplaceStateWarning';
+} from './LocationPathnameAlwaysSame.js';
+export { default as NoslashHashPathCoding } from './NoslashHashPathCoding.js';
+export { default as PushEncodedLocation } from './PushEncodedLocation.js';
+export { default as PushInvalidPathname } from './PushInvalidPathname.js';
+export { default as PushNewLocation } from './PushNewLocation.js';
+export { default as PushMissingPathname } from './PushMissingPathname.js';
+export { default as PushSamePath } from './PushSamePath.js';
+export { default as PushSamePathWarning } from './PushSamePathWarning.js';
+export { default as PushState } from './PushState.js';
+export { default as PushStateWarning } from './PushStateWarning.js';
+export { default as PushRelativePathname } from './PushRelativePathname.js';
+export { default as PushUnicodeLocation } from './PushUnicodeLocation.js';
+export { default as ReplaceInvalidPathname } from './ReplaceInvalidPathname.js';
+export { default as ReplaceNewLocation } from './ReplaceNewLocation.js';
+export { default as ReplaceSamePath } from './ReplaceSamePath.js';
+export { default as ReplaceState } from './ReplaceState.js';
+export { default as ReplaceStateWarning } from './ReplaceStateWarning.js';
 export {
   default as ReturnFalseTransitionHook
-} from './ReturnFalseTransitionHook';
-export { default as SlashHashPathCoding } from './SlashHashPathCoding';
-export { default as TransitionHookArgs } from './TransitionHookArgs';
+} from './ReturnFalseTransitionHook.js';
+export { default as SlashHashPathCoding } from './SlashHashPathCoding.js';
+export { default as TransitionHookArgs } from './TransitionHookArgs.js';
