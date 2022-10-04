5838a3ad86653d0a932ca2612ff3b103c5af16e1
Jelle Roets <jelle.roets@oqton.com>
2022-10-04 23:30:51 +0200

    add constant domain history

--- packages/history/index.ts
+++ packages/history/index.ts
@@ -239,10 +239,11 @@ export interface History {
    * current index. For example, a "back" navigation would use go(-1).
    *
    * @param delta - The delta in the stack index
+   * @returns blocked - A boolean that indicates if the navigation was blocked or not.
    *
    * @see https://github.com/remix-run/history/tree/main/docs/api-reference.md#history.go
    */
-  go(delta: number): void;
+  go(delta: number): boolean;
 
   /**
    * Navigates to the previous entry in the stack. Identical to go(-1).
@@ -250,16 +251,20 @@ export interface History {
    * Warning: if the current location is the first location in the stack, this
    * will unload the current document.
    *
+   * @returns blocked - A boolean that indicates if the navigation was blocked or not.
+   *
    * @see https://github.com/remix-run/history/tree/main/docs/api-reference.md#history.back
    */
-  back(): void;
+  back(): boolean;
 
   /**
    * Navigates to the next entry in the stack. Identical to go(1).
    *
+   * @returns blocked - A boolean that indicates if the navigation was blocked or not.
+   *
    * @see https://github.com/remix-run/history/tree/main/docs/api-reference.md#history.forward
    */
-  forward(): void;
+  forward(): boolean;
 
   /**
    * Sets up a listener that will be called whenever the current location
@@ -317,6 +322,14 @@ export interface MemoryHistory extends History {
   readonly index: number;
 }
 
+/**
+ * A constant domain history is similar to browser history, however it will block
+ * back and forward navigation if it would redirect you to a different domain.
+ *
+ * @see /docs/...
+ */
+export interface ConstantDomainHistory extends BrowserHistory {}
+
 const readOnly: <T>(obj: T) => Readonly<T> = __DEV__
   ? (obj) => Object.freeze(obj)
   : (obj) => obj;
@@ -346,6 +359,7 @@ type HistoryState = {
   usr: any;
   key?: string;
   idx: number;
+  fromDomain?: string;
 };
 
 const BeforeUnloadEventType = "beforeunload";
@@ -524,6 +538,7 @@ export function createBrowserHistory(
 
   function go(delta: number) {
     globalHistory.go(delta);
+    return false;
   }
 
   let history: BrowserHistory = {
@@ -538,10 +553,10 @@ export function createBrowserHistory(
     replace,
     go,
     back() {
-      go(-1);
+      return go(-1);
     },
     forward() {
-      go(1);
+      return go(1);
     },
     listen(listener) {
       return listeners.push(listener);
@@ -787,6 +802,7 @@ export function createHashHistory(
 
   function go(delta: number) {
     globalHistory.go(delta);
+    return false;
   }
 
   let history: HashHistory = {
@@ -801,10 +817,10 @@ export function createHashHistory(
     replace,
     go,
     back() {
-      go(-1);
+      return go(-1);
     },
     forward() {
-      go(1);
+      return go(1);
     },
     listen(listener) {
       return listeners.push(listener);
@@ -967,6 +983,8 @@ export function createMemoryHistory(
       index = nextIndex;
       applyTx(nextAction, nextLocation);
     }
+
+    return false;
   }
 
   let history: MemoryHistory = {
@@ -984,16 +1002,265 @@ export function createMemoryHistory(
     replace,
     go,
     back() {
-      go(-1);
+      return go(-1);
     },
     forward() {
-      go(1);
+      return go(1);
     },
     listen(listener) {
       return listeners.push(listener);
     },
     block(blocker) {
       return blockers.push(blocker);
+    },
+  };
+
+  return history;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Constant Domain History
+////////////////////////////////////////////////////////////////////////////////
+
+export type ConstantDomainHistoryOptions = { window?: Window };
+
+/**
+ * A constant domain history is similar to browser history, however it will block
+ * back and forward navigation if it would redirect you to a different domain.
+ *
+ */
+export function createConstantDomainHistory(
+  options: BrowserHistoryOptions = {}
+): BrowserHistory {
+  // TODO see if / how we can share common functionality with createBrowserHistory
+  // as most of this function is duplication only push, replace and go functions differ
+  let { window = document.defaultView! } = options;
+  let globalHistory = window.history;
+
+  function getIndexAndLocation(): [number, Location] {
+    let { pathname, search, hash } = window.location;
+    let state = globalHistory.state || {};
+    return [
+      state.idx,
+      readOnly<Location>({
+        pathname,
+        search,
+        hash,
+        state: state.usr || null,
+        key: state.key || "default",
+      }),
+    ];
+  }
+
+  let blockedPopTx: Transition | null = null;
+  function handlePop() {
+    if (blockedPopTx) {
+      blockers.call(blockedPopTx);
+      blockedPopTx = null;
+    } else {
+      let nextAction = Action.Pop;
+      let [nextIndex, nextLocation] = getIndexAndLocation();
+
+      if (blockers.length) {
+        if (nextIndex != null) {
+          let delta = index - nextIndex;
+          if (delta) {
+            // Revert the POP
+            blockedPopTx = {
+              action: nextAction,
+              location: nextLocation,
+              retry() {
+                go(delta * -1);
+              },
+            };
+
+            go(delta);
+          }
+        } else {
+          // Trying to POP to a location with no index. We did not create
+          // this location, so we can't effectively block the navigation.
+          warning(
+            false,
+            // TODO: Write up a doc that explains our blocking strategy in
+            // detail and link to it here so people can understand better what
+            // is going on and how to avoid it.
+            `You are trying to block a POP navigation to a location that was not ` +
+              `created by the history library. The block will fail silently in ` +
+              `production, but in general you should do all navigation with the ` +
+              `history library (instead of using window.history.pushState directly) ` +
+              `to avoid this situation.`
+          );
+        }
+      } else {
+        applyTx(nextAction);
+      }
+    }
+  }
+
+  window.addEventListener(PopStateEventType, handlePop);
+
+  let action = Action.Pop;
+  let [index, location] = getIndexAndLocation();
+  let listeners = createEvents<Listener>();
+  let blockers = createEvents<Blocker>();
+
+  if (index == null) {
+    index = 0;
+    globalHistory.replaceState({ ...globalHistory.state, idx: index }, "");
+  }
+
+  function createHref(to: To) {
+    return typeof to === "string" ? to : createPath(to);
+  }
+
+  // state defaults to `null` because `window.history.state` does
+  function getNextLocation(to: To, state: any = null): Location {
+    return readOnly<Location>({
+      pathname: location.pathname,
+      hash: "",
+      search: "",
+      ...(typeof to === "string" ? parsePath(to) : to),
+      state,
+      key: createKey(),
+    });
+  }
+
+  function getHistoryStateAndUrl(
+    nextLocation: Location,
+    index: number,
+    fromDomain?: string
+  ): [HistoryState, string] {
+    return [
+      {
+        usr: nextLocation.state,
+        key: nextLocation.key,
+        idx: index,
+        fromDomain,
+      },
+      createHref(nextLocation),
+    ];
+  }
+
+  function allowTx(action: Action, location: Location, retry: () => void) {
+    return (
+      !blockers.length || (blockers.call({ action, location, retry }), false)
+    );
+  }
+
+  function applyTx(nextAction: Action) {
+    action = nextAction;
+    [index, location] = getIndexAndLocation();
+    listeners.call({ action, location });
+  }
+
+  function push(to: To, state?: any) {
+    let nextAction = Action.Push;
+    let nextLocation = getNextLocation(to, state);
+    function retry() {
+      push(to, state);
+    }
+
+    if (allowTx(nextAction, nextLocation, retry)) {
+      // On push: always set the fromDomain prop to the current domain
+      let [historyState, url] = getHistoryStateAndUrl(
+        nextLocation,
+        index + 1,
+        window.location.hostname
+      );
+
+      // TODO: Support forced reloading
+      // try...catch because iOS limits us to 100 pushState calls :/
+      try {
+        globalHistory.pushState(historyState, "", url);
+      } catch (error) {
+        // They are going to lose state here, but there is no real
+        // way to warn them about it since the page will refresh...
+        window.location.assign(url);
+      }
+
+      applyTx(nextAction);
+    }
+  }
+
+  function replace(to: To, state?: any) {
+    let nextAction = Action.Replace;
+    let nextLocation = getNextLocation(to, state);
+    function retry() {
+      replace(to, state);
+    }
+
+    if (allowTx(nextAction, nextLocation, retry)) {
+      // On replace: retain the domain from the location to be replaced
+      let [historyState, url] = getHistoryStateAndUrl(
+        nextLocation,
+        index,
+        globalHistory.state?.fromDomain
+      );
+
+      // TODO: Support forced reloading
+      globalHistory.replaceState(historyState, "", url);
+
+      applyTx(nextAction);
+    }
+  }
+
+  function go(delta: number) {
+    if (delta !== -1) {
+      warning(
+        false,
+        `go(delta) with a delta != -1 cannot be used as it can't be verified you'll end up on the same domain`
+      );
+      // return true to notify the navigation was blocked
+      return true;
+    }
+    // In case go back 1 entry, check if we came from the same domain
+    if (globalHistory.state?.fromDomain === window?.location.hostname) {
+      // It is safe to go back since we know we came from the same domain
+      globalHistory.go(delta);
+      return false;
+    } else {
+      // block going back to a different domain
+      return true;
+    }
+  }
+
+  let history: BrowserHistory = {
+    get action() {
+      return action;
+    },
+    get location() {
+      return location;
+    },
+    createHref,
+    push,
+    replace,
+    go,
+    back() {
+      return go(-1);
+    },
+    forward() {
+      return go(1);
+    },
+    listen(listener) {
+      return listeners.push(listener);
+    },
+    block(blocker) {
+      let unblock = blockers.push(blocker);
+
+      if (blockers.length === 1) {
+        window.addEventListener(BeforeUnloadEventType, promptBeforeUnload);
+      }
+
+      return function () {
+        unblock();
+
+        // Remove the beforeunload listener so the document may
+        // still be salvageable in the pagehide event.
+        // See https://html.spec.whatwg.org/#unloading-documents
+        if (!blockers.length) {
+          window.removeEventListener(BeforeUnloadEventType, promptBeforeUnload);
+        }
+      };
     },
   };
 
